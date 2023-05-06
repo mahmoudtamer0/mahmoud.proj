00b4d90d200d2612f40802d0f78c4b9148c02426
mahmoud <mamoidtamer300@gmail.com>
2023-05-06 03:48:10 +0300

    editing

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 let skills = document.querySelector('.skills')
 let spans = document.querySelectorAll('.skills span')
 
-// window.onscroll = function {
-//     if (window.scrollY >= skills.offsetTop - 200) {
-//         spans.forEach((span) => {
-//             span.style.width = span.dataset.width
-//         })
-//     } else if (window.scrollY > 11570.400390625) {
-//         spans.forEach((span) => {
-//             span.style.width = 0;
-//         })
-//     }
-//     if (window.scrollY >= secs.offsetTop) {
-//         if (!started) {
-//             nums.forEach((number) => startcount(number))
-//         }
-//         started = true;
-//     }
-// };
+window.onscroll = function () {
+    if (window.scrollY >= skills.offsetTop - 200) {
+        spans.forEach((span) => {
+            span.style.width = span.dataset.width
+        })
+    } else if (window.scrollY > 11570.400390625) {
+        spans.forEach((span) => {
+            span.style.width = 0;
+        })
+    }
+    if (window.scrollY >= secs.offsetTop) {
+        if (!started) {
+            nums.forEach((number) => startcount(number))
+        }
+        started = true;
+    }
+};
 let countdat = new Date("Dec 31, 2023 12:59:59").getTime()
 let counter = setInterval(() => {
     let dateNow = new Date().getTime();
@@ -52,39 +52,53 @@ let skills_btn = document.getElementById('skills_btn')
 let articles = document.querySelector('.articles')
 let gallery = document.querySelector('.gallery')
 let features = document.querySelector('.features')
+let testi = document.querySelector('.testi')
+let team = document.querySelector('.team')
+let works = document.querySelector('.works')
 
-window.onscroll = function () {
-    if (window.scrollY >= skills.offsetTop - 200 && window.scrollY < 7110.39990234375) {
-        spans.forEach((span) => {
-            span.style.width = span.dataset.width
-        })
-    } else if (window.scrollY > 7110.39990234375) {
-        spans.forEach((span) => {
-            span.style.width = 0;
-        })
-    }
-    if (window.scrollY >= secs.offsetTop) {
-        if (!started) {
-            nums.forEach((number) => startcount(number))
-        }
-        started = true;
-    }
+// window.onscroll = function () {
+// if (window.scrollY >= skills.offsetTop - 80 && window.scrollY < (team.offsetTop - 100)) {
+//     spans.forEach((span) => {
+//         span.style.width = span.dataset.width
+//     })
+// }
+// } else if (window.scrollY > 7110.39990234375) {
+//     spans.forEach((span) => {
+//         span.style.width = 0;
+//     })
+// if (window.scrollY >= secs.offsetTop) {
+//     if (!started) {
+//         nums.forEach((number) => startcount(number))
+//     }
+//     started = true;
+// }
 
-    if (window.scrollY >= 745.5999755859375 && window.scrollY < 1792) {
-        artic_btn.classList.add('activ')
-    } else if (window.scrollY >= 2956 && window.scrollY < 3869.600097656255) {
-        feat_btn.classList.add('activ')
-    } else if (window.scrollY >= gallery.offsetTop && window.scrollY < 2855.199951171875) {
-        gallery_btn.classList.add('activ')
-    } else if (window.scrollY >= skills.offsetTop - 200 && window.scrollY < 7110.39990234375) {
-        skills_btn.classList.add('activ')
-    } else {
-        artic_btn.classList.remove('activ')
-        feat_btn.classList.remove('activ')
-        gallery_btn.classList.remove('activ')
-        skills_btn.classList.remove('activ')
-        spans.forEach((span) => {
-            span.style.width = 0;
-        })
-    }
-}
+
+// if (window.scrollY >= articles.offsetTop) {
+//     artic_btn.classList.add('activ')
+
+//     // } else if (window.scrollY >= (features.offsetTop - 80) && window.scrollY < (testi.offsetTop - 100)) {
+//     //     feat_btn.classList.add('activ')
+//     // } else if (window.scrollY >= (gallery.offsetTop - 80) && window.scrollY < features.offsetTop - 80) {
+//     //     gallery_btn.classList.add('activ')
+// } else if (window.scrollY >= gallery.offsetTop - 80) {
+//     gallery_btn.classList.add('activ')
+//     artic_btn.classList.add('activ')
+// }
+// else if (window.scrollY >= features.offsetTop - 80) {
+//     gallery_btn.classList.remove('activ')
+//     artic_btn.classList.remove('activ')
+// }
+// //  else if (window.scrollY >= (skills.offsetTop - 80) && window.scrollY < (works.offsetTop - 100)) {
+// //     skills_btn.classList.add('activ')
+
+// else {
+//     artic_btn.classList.remove('activ')
+//     feat_btn.classList.remove('activ')
+//     gallery_btn.classList.remove('activ')
+//     skills_btn.classList.remove('activ')
+//     // spans.forEach((span) => {
+//     //     span.style.width = 0;
+//     // })
+// }
+// }
